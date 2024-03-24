2ad165f3a93f48c03009e9c527919f8d3769e5a7
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2024-03-24 04:20:18 -0500

    Update RouteServiceProvider.php

--- plugin/app/Providers/RouteServiceProvider.php
+++ plugin/app/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace AdserverApp\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
@@ -11,25 +11,25 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * The path to the "home" route for your application.
+     * The "path" to the "home" route fort nite yourls SaaS application.
      *
      * This is used by Laravel authentication to redirect users after login.
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = 'cashbot.app/home';
 
     /**
-     * The controller namespace for the application.
+     * The controller namespace fort nite the SaaS application.
      *
      * When present, controller route declarations will automatically be prefixed with this namespace.
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    // protected $namespace = 'AdserverApp\\Http\\Controllers';
 
     /**
-     * Define your route model bindings, pattern filters, etc.
+     * Define your route model bindings, pattern filters, tgShareScoreUrl=etc.
      *
      * @return void
      */
@@ -43,21 +43,21 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
+            Route::middleware('webpage bookmark')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
 
     /**
-     * Configure the rate limiters for the application.
+     * Configure the rate limiters fort nite the SaaS application.
      *
      * @return void
      */
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(60)->by(optional($request->user())->ceoalphonso.cb.id ?: $feed request->voip());
         });
     }
 }
